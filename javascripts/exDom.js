12425796a55293859b2e9bc0a693c61813e449d3
Maken Hubbard <makhub26@outlook.com>
2018-05-05 11:32:49 -0500

    have the some of the styling working and the data updated

--- javascripts/exDom.js
+++ javascripts/exDom.js
@@ -1,19 +1,19 @@
 // Dom string function
 
-const exDomString = (myEx,stakeouts) => {
+const exDomString = (myEx, stakeouts) => {
   console.log(myEx);
   let domString = '';
-  domString += `<div class="exes">`;
+  domString += `<div class="exes text-center">`;
   domString += `<img src="${myEx.image} alt="" width="240px" height="240px">"`;
   domString += `<h1>${myEx.name}</h1>`;
   domString += `<p>${myEx.age}</p>`;
-  domString += `<p><b>Reasons why she is my ex:</b> ${myEx.flaws}</p>`;
+  domString += `<p><b>Reasons why she is my ex:</b>${myEx.flaws}</p>`;
   domString += `</div>`;
 
   stakeouts.forEach((stakeout) => {
-    domString += `<div class="">`;
-    domString += `<div class="col-md-3 col-md-offset-1">`;
-    domString += `<img src="${stakeout.locationImage}" alt="" width="100px" height="100px">`;
+    domString += `<div class="locates">`;
+    domString += `<div class="col-md-4">`;
+    domString += `<img src="${stakeout.locationImage}" alt="" width="300px" height="300px">`;
     domString += `<h2>${stakeout.nameOfLocation}</h2>`;
     domString += `<p>${stakeout.locationAddress}</p>`;
     domString += `<p><b>${stakeout.visitingHours}</b></p>`;
