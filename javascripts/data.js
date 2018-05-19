f8f632474b8bdc2e1f74335a55a0fa54e3f35d00
Maken Hubbard <makhub26@outlook.com>
2018-05-19 14:22:48 -0500

    have the data coming back the way I want just need to apply it to an event and and domString builder function wip

--- javascripts/data.js
+++ javascripts/data.js
@@ -20,12 +20,48 @@ const bothData = () => {
       dom.locationDomString(locals);
       return loadEx()
         .then((exes) => {
+          dom.exDomString(exes);
+          return Promise.resolve(exes,locals);
+        });
+    });
+};
+
+const smashedData = () => {
+  // let exes = [];
+  // let locals = [];
+  // let excations = [];
+  return loadLocations()
+    .then((locals) => {
+      // console.error(result);
+      // locals = [...result,];
+      dom.locationDomString(locals);
+      return loadEx()
+        .then((exes) => {
           // console.error(result2);
           // exes = [...result2,];
           dom.exDomString(exes);
-          // return Promise.resolve(exes,locals);
+          const exPlusLocations = dataSmash(exes,locals);
+          return Promise.resolve(exPlusLocations);
         });
     });
+};
+
+const dataSmash = (exes, locals) => {
+  const allData = [];
+  // this is where the smash work happens
+  exes.forEach((ex) => {
+    const finalEx = ex;
+    finalEx.relatedLocations = [];
+    for (let i = 0; i < ex.locations.length; i++) {
+      locals.forEach((local) => {
+        if (ex.locations[i] === local.locationId) {
+          finalEx.relatedLocations.push(local);
+        };
+      });
+    };
+    allData.push(finalEx);
+  });
+  return allData;
 };
 
 const initializer = () => {
@@ -34,4 +70,5 @@ const initializer = () => {
 
 module.exports = {
   initializer,
+  smashedData,
 };
--- javascripts/main.js
+++ javascripts/main.js
@@ -2,4 +2,7 @@ const data = require('./data');
 const events = require('./events');
 
 data.initializer();
+data.smashedData().then((results) => {
+  console.error(results);
+});
 events();
